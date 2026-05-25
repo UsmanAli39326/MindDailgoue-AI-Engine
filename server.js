34e8b3f9f38199e14b7b3fb25ee8e67a63bf33e8
UsmanAli39326 <70139326@student.uol.edu.pk>
2026-05-25 06:01:36 -0700

    fixed limit size

--- server.js
+++ server.js
@@ -33,7 +33,8 @@ const PORT = process.env.PORT || 8000;
 // Middleware
 app.use(requestLogger);
 app.use(cors());
-app.use(express.json({ limit: '16kb' }));
+app.use(express.json({ limit: '5mb' }));
+app.use(express.urlencoded({ limit: '5mb', extended: true }));
 app.use(express.static('public')); // Serve static assets like avatars
 
 // 1. Health check — fully public
